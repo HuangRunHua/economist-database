024f874ee3894f27b23f6902021344706b0a241b
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-04-25 20:39:47 +0800

    bug fixes

--- Daily News/Daily News/YouTubeView/YouTubeVideoView.swift
+++ Daily News/Daily News/YouTubeView/YouTubeVideoView.swift
@@ -9,43 +9,26 @@ import SwiftUI
 
 struct YouTubeVideoView: View {
     
-    let youtubeVideoPath: String
-    @State private var youtubeVideoID: String? = nil
+    let youtubeVideoID: String
     
     var body: some View {
         VStack {
-            if let youtubeVideoID {
-                VStack {
-                    YouTubeVideo(videoID: youtubeVideoID)
-                        .aspectRatio(4/3, contentMode: .fit)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-                .frame(maxWidth: .infinity)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 0)
-                        .stroke(Color.boundColor, lineWidth: 2)
-                )
-                .cornerRadius(0)
-            } else {
-                EmptyView()
-            }
+            YouTubeVideo(videoID: youtubeVideoID)
+                .aspectRatio(4/3, contentMode: .fit)
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .onAppear {
-            self.youtubeVideoID = paraseYoutubeID(source: self.youtubeVideoPath)
-        }
-    }
-    
-    func paraseYoutubeID(source link: String) -> String? {
-        if let equalIndex = link.firstIndex(of: "=") {
-            return String(link[link.index(equalIndex, offsetBy: 1)..<link.endIndex])
-        }
-        return nil
+        .frame(maxWidth: .infinity)
+        .overlay(
+            RoundedRectangle(cornerRadius: 0)
+                .stroke(Color.boundColor, lineWidth: 2)
+        )
+        .cornerRadius(0)
     }
 }
 
 
 struct YouTubeVideoView_Previews: PreviewProvider {
     static var previews: some View {
-        YouTubeVideoView(youtubeVideoPath: "https://www.youtube.com/watch?v=aZ-FipkmTMg")
+        YouTubeVideoView(youtubeVideoID: "3tJUflhYIpo")
     }
 }
