d6c3cec70dd694497acf1f4176869492fc8c3939
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-05-11 22:22:12 +0800

    Add 2023-05-13

--- weekly_paraser.py
+++ weekly_paraser.py
@@ -145,6 +145,10 @@ if __name__ == "__main__":
         {
             "link": "https://www.economist.com/weeklyedition/2023-05-06",
             "id": "00000000-0000-0000-0000-000000000017"
+        },
+        {
+            "link": "https://www.economist.com/weeklyedition/2023-05-13",
+            "id": "00000000-0000-0000-0000-000000000018"
         }
     ]
 
