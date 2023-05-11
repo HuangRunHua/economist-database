5d83fb42aa02e6e7e7976dbe75029a875559178d
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-05-11 16:32:32 +0800

    update for macOS

--- Daily News/Daily News/Views/ArticleView.swift
+++ Daily News/Daily News/Views/ArticleView.swift
@@ -67,7 +67,6 @@ struct ArticleView: View {
                                         self.translateText = self.currentArticle.hashTag
                                     })
                                 }))
-                                
                             Spacer()
                         }
                         HStack {
