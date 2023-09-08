1174f76054379931144c71379a4640bbd29f1c1a
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-09-08 10:32:09 +0800

    Add 2023-09-09

--- add_id_from_weekly-json.py
+++ add_id_from_weekly-json.py
@@ -175,6 +175,10 @@ if __name__ == "__main__":
         {
             "link": "https://www.economist.com/weeklyedition/2023-09-02",
             "id": "00000000-0000-0000-0000-000000000032"
+        },
+        {
+            "link": "https://www.economist.com/weeklyedition/2023-09-09",
+            "id": "00000000-0000-0000-0000-000000000033"
         }
     ]
 
--- article_paraser_manual.py
+++ article_paraser_manual.py
@@ -135,5 +135,5 @@ class MagazineGenerator(object):
 
 
 if __name__ == "__main__":
-    article_analyzer = MagazineGenerator(folder="2023-09-02")
+    article_analyzer = MagazineGenerator(folder="2023-09-09")
     article_analyzer.generat_jsons()
