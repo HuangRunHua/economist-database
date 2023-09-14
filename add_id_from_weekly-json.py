04f963fc3534c1d17ea7b1cf81123efc931ab99c
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2023-09-14 22:47:07 +0800

    Add 2023-09-16

--- add_id_from_weekly-json.py
+++ add_id_from_weekly-json.py
@@ -179,6 +179,10 @@ if __name__ == "__main__":
         {
             "link": "https://www.economist.com/weeklyedition/2023-09-09",
             "id": "00000000-0000-0000-0000-000000000033"
+        },
+        {
+            "link": "https://www.economist.com/weeklyedition/2023-09-16",
+            "id": "00000000-0000-0000-0000-000000000034"
         }
     ]
 
