832cc0a207ecf3c305d9413be5134707a076040a
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2024-01-11 22:47:17 +0800

    更新数据库

--- add_id_from_weekly-json.py
+++ add_id_from_weekly-json.py
@@ -243,6 +243,10 @@ if __name__ == "__main__":
         {
             "link": "https://www.economist.com/weeklyedition/2024-01-06",
             "id": "00000000-0000-0000-0000-000000000049"
+        },
+        {
+            "link": "https://www.economist.com/weeklyedition/2024-01-13",
+            "id": "00000000-0000-0000-0000-000000000050"
         }
     ]
 
